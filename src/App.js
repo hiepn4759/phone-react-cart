c7f2ed46e030280ce0e796d8484894f853490410
hiepn4759 <hiepn4759@gmail.com>
2019-08-20 09:15:22 +0700

    chia component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
 
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        
-      </div>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <ProductList></ProductList>
+        <Details></Details>
+        <Cart></Cart>
+        <Default></Default>
+      </React.Fragment>
     );
   }
 }
